61ea707e1245a2ae2fae819b723da6c27ef6290d
Matias Santillan <masantillan@frba.utn.edu.ar>
2023-09-17 00:03:56 -0300

    Fixes in Logic Game, Screen and logic of Rules add

--- app.js
+++ app.js
@@ -9,16 +9,24 @@ const items = document.querySelectorAll(".item")
 const textsPickeds = document.querySelectorAll(".textPicked")
 const playAgain = document.querySelector(".playAgain")
 const point = document.querySelector(".points")
+const btnHouse = document.getElementById("btnHouse")
+const btnYou = document.getElementById("btnYou")
 
 const arrayClass = [
     "paper",
     "scissor",
-    "rock"
+    "rock",
+    "blue",
+    "yellow",
+    "red"
 ]
 
 let gameInProgr = true;
 let classYouPicked = ""
 let classHousePicked = ""
+let colorHousePicked = ""
+let colorYouPicked = ""
+let numRandom = 0
 
 btns.forEach(btn => {
     btn.addEventListener("click", ()=>{
@@ -30,69 +38,64 @@ btns.forEach(btn => {
                 txtP.classList.remove("dp-none")
             })
 
-            setClassYouPicked(btn)
-                        
+            const insideButton = btn.querySelector(".item");
+            btn.classList.remove("blue")
+            if (insideButton.classList.contains("paper")) {
+                classYouPicked = "paper";
+            } else if (insideButton.classList.contains("scissor")) {
+                classYouPicked = "scissor";
+            } else if (insideButton.classList.contains("rock")) {
+                classYouPicked = "rock";
+            }
+
+
             items.forEach(i=>{
                 i.classList.remove("scissor")
                 i.classList.remove("paper")
             })
             
-            setClassHousePicked()
-
+            numRandom = Math.floor(Math.random()*3);
+            classHousePicked = arrayClass[numRandom]
+            colorHousePicked = arrayClass[numRandom+3]
+            btnHouse.classList.remove("yellow")
+            btnHouse.classList.add(colorHousePicked)
             housePicked.classList.add(classHousePicked);
             youPicked.classList.add(classYouPicked)
 
-            showResult(arrayClass[numRandom], classYouPicked)
+            if(classYouPicked == "paper"){
+                colorYouPicked = "blue"
+            } else if(classYouPicked == "scissor"){
+                colorYouPicked = "yellow"
+            } else {
+                colorYouPicked = "red"
+            }
+            btnYou.classList.add(colorYouPicked)
             
+            const textResult = document.querySelector(".dlw")
+            if (classHousePicked === classYouPicked) {
+                textResult.innerHTML = "DRAW";
+            } else if (
+                (classYouPicked === "paper" && classHousePicked === "rock") ||
+                (classYouPicked === "scissor" && classHousePicked === "paper") ||
+                (classYouPicked === "rock" && classHousePicked === "scissor")
+            ) {
+                textResult.innerHTML = "YOU WON";
+                point.innerHTML = parseInt(point.textContent) + 1;
+            } else {
+                textResult.innerHTML = "YOU LOSE";
+                if(point.textContent != "0"){
+                    point.innerHTML = parseInt(point.textContent) - 1;
+                }
+            }
             gameInProgr = false;
         }
-        changeCursor()
+        addCursorAuto()
     })
 })
-
-function setClassYouPicked(btn){
-    const insideButton = btn.querySelector(".item");
-    if (insideButton.classList.contains("paper")) {
-        classYouPicked = "paper";
-    } else if (insideButton.classList.contains("scissor")) {
-        classYouPicked = "scissor";
-    } else if (insideButton.classList.contains("rock")) {
-        classYouPicked = "rock";
-    }
-}
-
-function setClassHousePicked() {
-    let numRandom = Math.floor(Math.random()*3);
-    classHousePicked = arrayClass[numRandom]
-}
-
-showResult = (classHousePick, classYouPick)  => {
-    const textResult = document.querySelector(".dlw")
-    if (classHousePick === classYouPick) {
-        textResult.innerHTML = "DRAW";
-    } else if (
-        (classYouPick === "paper" && classHousePick === "rock") ||
-        (classYouPick === "scissor" && classHousePick === "paper") ||
-        (classYouPick === "rock" && classHousePick === "scissor")
-    ) {
-        textResult.innerHTML = "YOU WON";
-        point.innerHTML = parseInt(point.textContent) + 1;
-    } else {
-        textResult.innerHTML = "YOU LOSE";
-        point.textContent = "0"
-    }
-}
-
 
 function addCursorAuto() {
     btns.forEach(btn => {
         btn.classList.add("cursorAuto")
-    })
-}
-
-function removeCursorAuto() {
-    btns.forEach(btn => {
-        btn.classList.remove("cursorAuto")
     })
 }
 
@@ -103,13 +106,46 @@ playAgain.addEventListener("click", () => {
     main.classList.remove("change-mgBottom", "bgImage-none")
     housePicked.classList.remove(classHousePicked);
     youPicked.classList.remove(classYouPicked)
+    btnHouse.classList.remove(colorHousePicked)
+    btnYou.classList.remove(colorYouPicked)
     classYouPicked = ""
     classHousePicked = ""
+    numRandom =  0
+    colorHousePicked = ""
     textsPickeds.forEach(txtP => {
         txtP.classList.add("dp-none")
     })
     items.forEach((item, index)=>{
         item.classList.add(arrayClass[index])
     })
+    btns.forEach((btn, index)=>{
+        if(index<3){
+            btn.classList.add(arrayClass[index+3])
+        }
+    })
     removeCursorAuto()
+})
+
+function removeCursorAuto() {
+    btns.forEach(btn => {
+        btn.classList.remove("cursorAuto")
+    })
+}
+
+const btnRules = document.querySelector(".btnRules")
+const rules = document.querySelector(".rules")
+const btnClose = document.querySelector(".imgClose")
+
+btnRules.addEventListener("click", ()=>{
+    rules.classList.remove("dp-none")
+    rules.classList.add("bottomToTop")
+    rules.classList.remove("topToBottom")
+})
+
+btnClose.addEventListener("click", ()=>{
+    rules.classList.remove("bottomToTop")
+    rules.classList.add("topToBottom")
+    setTimeout(() => {
+        rules.classList.add("dp-none");
+    }, 500);
 })
